977664a7995eafcebf3e771eee55a99e47191ca9
jokerxin <jxpro@qq.com>
2023-03-20 12:45:38 +0800

    Add MacOS environment

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import requests
 
@@ -7,24 +6,15 @@ from xyw_class import XYW
 
 
 os.environ['NO_PROXY'] = 'xyw.hainanu.edu.cn'
-net = XYW(r'/Users/xin/Documents/Program/Python/auto-login/src')
 
-while True:
+try:
+    requests.get('https://www.baidu.com')
+except requests.exceptions.RequestException:
+    print('网络未认证')
+    net = XYW(r'/Users/xin/Program/Python/auto-login/src')
     try:
-        requests.get('https://www.baidu.com')
-        print('网络已连通')
-        break
-    except requests.exceptions.SSLError as ssl_error:
-        print('网络未认证')
-        results = net.connect()
-        # region 输出中间结果
-        # for result in results:
-        #     if isinstance(result, tuple):
-        #         for item in result:
-        #             print(item)
-        #     else:
-        #         print(result)
-        # endregion
-    except requests.exceptions.ConnectionError as connection_error:
-        print('网络未连接')
-        time.sleep(1)
+        net.connect()
+    except requests.exceptions.RequestException:
+        print('网络认证失败')
+
+print('网络已连通')
