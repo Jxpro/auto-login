a013e994e248c976135b3bf42ed96aaa70ff5119
jokerxin <jxpro@qq.com>
2023-03-20 23:11:48 +0800

    Fix status error

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ os.environ['NO_PROXY'] = 'xyw.hainanu.edu.cn'
 
 try:
     requests.get('https://www.baidu.com')
+    print('网络已连通')
 except requests.exceptions.RequestException:
     print('网络未认证')
     net = XYW(r'/Users/xin/Program/Python/auto-login/src')
     try:
         net.connect()
+        print('网络已连通')
     except requests.exceptions.RequestException:
         print('网络认证失败')
-
-print('网络已连通')
